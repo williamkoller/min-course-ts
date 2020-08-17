9b6b657d2061db02e6fd5f88862d63bee75450f7
William Koller <wkoller25@gmail.com>
2020-08-17 01:39:40 -0300

    feat: add model users

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
-
-type User = {
-  name: string
-  lastName: string
-}
-
-const user: User = {
-  name: 'William',
-  lastName: 'Koller'
+import { Users } from './model/user' 
+const user: Users = {
+  firstName: 'William',
+  lastName: 'Koller',
+  age: 31,
+  weight: 92,
+  created_at: new Date()
 }
 
 console.log(user)
